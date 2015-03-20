49b0e625df97d268bb31eeb19c438e96a8576464
kwwaks <ken.weinberg@mercadolibre.com>
2015-03-20 09:32:34 -0300

    Agregado el atributo de nombre a la clase Usuario

--- grails-app/domain/melistats/Usuario.groovy
+++ grails-app/domain/melistats/Usuario.groovy
@@ -1,6 +1,7 @@
 package melistats
 
 class Usuario {
+	String nombre
 	String nombreUsuario
 	static hasMany = [busquedas:Busqueda,preferencias:Preferencia]
 	static constraints = { busquedas nullable: true;
