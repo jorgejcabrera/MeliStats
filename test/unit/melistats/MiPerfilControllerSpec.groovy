2a3a20a95998b7a238cc97c22a17a3e46426d9d1
candecp <cande.cp@gmail.com>
2015-03-20 13:57:02 -0300

    borre controller de preferencia y cree el de mi perfil y la view

--- test/unit/melistats/MiPerfilControllerSpec.groovy
+++ test/unit/melistats/MiPerfilControllerSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(PreferenciaController)
-class PreferenciaControllerSpec extends Specification {
+@TestFor(MiPerfilController)
+class MiPerfilControllerSpec extends Specification {
 
     def setup() {
     }
