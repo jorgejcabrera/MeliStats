a131a4db663b767d77e231273328199d3dc4ce89
kwwaks <ken.weinberg@mercadolibre.com>
2015-03-31 18:13:40 -0300

    Armando el test para verificar si las preferencias se editan bien, sin terminar!!

--- test/unit/melistats/PreferenciaServiceSpec.groovy
+++ test/unit/melistats/PreferenciaServiceSpec.groovy
@@ -1,5 +1,6 @@
 package melistats
 
+import melistats.*
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -10,11 +11,32 @@ import spock.lang.Specification
 class PreferenciaServiceSpec extends Specification {
 
     def setup() {
+    	def nuevaPref = new Preferencia(nombrePref:"hola", precioPref:10, envioPref:30, reputacionPref:15, condicionPref:25)
+    	mockDomain(Preferencia, [Preferencia])
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-    }
+
+    void "Las prefencias se editan correctamente"() {
+		given:
+			def nombrePref = "NewName"
+			def precioPref = "30"
+			def envioPref = "40"
+			def reputacionPref = "35"
+			def condicionPref = "45"
+			def idPreferencia = 0
+			def datos = [nombrePref:nombrePref, precioPref:precioPref, envioPref:envioPref, reputacionPref:reputacionPref, condicionPref:condicionPref, idPreferencia: idPreferencia]
+		
+		when:
+			def result = service.editarPreferencia(datos)
+		
+		then:
+			result.nombrePref == "NewName"
+			result.precioPref == 30
+			result.envioPref == 40
+			result.reputacionPref == 35
+			result.condicionPref == 45
+	}
 }
